380480b5c275271aff332d74756b4792276858b3
shravanbireddy <shravanbireddy@10.2.67.157>
2022-05-22 12:11:33 -0400

    first commit

---
+++ LearningJava/src/FirstCode.java
@@ -0,0 +1,43 @@
+import java.util.Iterator;
+
+public class FirstCode  
+{
+	public static void main(String args[]) throws Exception
+	{
+			
+			
+		Thread t1=new Thread( () -> {
+			for(int i=0;i<5;i++) {
+				System.out.println("hi");
+				
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			
+});
+		Thread t2=new Thread(()-> {
+			
+
+			for(int i=0;i<5;i++) {
+				System.out.println("hello");
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		
+		
+			});
+		
+		
+	
+	
+	}
+
+}
